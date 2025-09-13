aef62fe2637f363cd0092cc2e5081e2fb94436a1
subhajit saha <saha3475@gmail.com>
2025-09-13 09:25:59 +0530

    simple program

---
+++ main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("java program for jenkins testing");
+        System.out.println("current date time is : " + new Date());
+    }
+}
